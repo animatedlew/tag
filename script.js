580e8935f3dc10d5ee338ae48657854690249ef7
Lewis Moronta <lewis.moronta@gmail.com>
2016-10-03 22:55:25 -0400

    cleanup

--- script.js
+++ script.js
@@ -1,6 +1,6 @@
 ref => {
-	var xhr = new XMLHttpRequest();
-	xhr.onload = e => Function(`return ${xhr.response}`).call(this)(ref);
-	xhr.open("GET", "img.js");
-	xhr.send();
+  var xhr = new XMLHttpRequest();
+  xhr.onload = e => Function(`return ${xhr.response}`).call(this)(ref);
+  xhr.open("GET", "img.js");
+  xhr.send();
 };
